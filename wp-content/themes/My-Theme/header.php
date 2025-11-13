25416e5d13933f81e3687b76f0dac8563fec3b15
DESKTOP-LN7SD4G\user <user>
2025-11-13 18:10:09 -0300

    fix: missing header info

--- wp-content/themes/My-Theme/header.php
+++ wp-content/themes/My-Theme/header.php
@@ -5,10 +5,11 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="stylesheet" href="/wp-content/themes/My-Theme/style.css">
-	<?php wp_head(); ?>
+   <title><?php bloginfo('name'); ?> &raquo; <?php is_front_page() ? bloginfo('description') : wp_title(''); ?></title>
+   <meta charset="<?php bloginfo( 'charset' ); ?>">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
 
